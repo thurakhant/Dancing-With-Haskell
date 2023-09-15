2e30962525d76e5302cea0b6c400d9d629b7e135
Thura Khant Thein <thurakhant@gmail.com>
2023-09-15 17:32:11 +0630

    add: Baby Function

---
+++ baby.hs
@@ -0,0 +1,13 @@
+-- Note
+-- [ Alert ] When you change any code compling again
+
+-- DoubleMe Function
+-- x is arguments
+doubleMe x = x + x
+
+-- DoubleUs Function
+doubleUs x y = doubleMe x + doubleMe y
+
+-- This is a if else statement
+doubleSmallNumber x = (if x > 100 then x else x * 2) + 1
+
